f039fa82c94a5c7fe66c83e5592fb306c5103000
Lethabo <68422133+lethabolekoloane@users.noreply.github.com>
2021-01-25 18:20:42 +0200

    Add files via upload

---
+++ dist/precache-manifest.bbe097cc252ed40b62f716415f2a26df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80021e8c1a3a386df55e",
+    "url": "/css/app.bd70d789.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "960040221808ddd5342c89be0e000f2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8610e7df79d80c0c835",
+    "url": "/js/about.90b284f2.js"
+  },
+  {
+    "revision": "80021e8c1a3a386df55e",
+    "url": "/js/app.06bede4c.js"
+  },
+  {
+    "revision": "6a9eeb8c188fca1a496a",
+    "url": "/js/chunk-vendors.31c752b6.js"
+  },
+  {
+    "revision": "512457f28f28149bb93af3176dfc42bb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
